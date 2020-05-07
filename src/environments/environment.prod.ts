96a8f0a23f4aee0f155f85fe14cca395620483f7
Ignacio <belmonteperona@gmail.com>
2020-05-07 05:00:39 +0200

    change ip to local

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  url:'http://play.bitcraft.es:3000'
+  url:'http://192.168.100.102:3000'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  url:'http://play.bitcraft.es:3000'
+  url:'http://192.168.100.102:3000'
 };
 
 /*
